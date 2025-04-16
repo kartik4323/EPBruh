67f7923e1f7411c558c08fddc08d2490ae880277
kartik4323 <pandeykartik4323@gmail.com>
2025-04-17 00:35:39 +0530

    oiyo

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,9 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/layout/Navbar";
 import Footer from "./components/layout/Footer";
 import Home from "./pages/Home";
-// import ServiceDetails from "./pages/ServiceDetails";
-// import About from "./pages/About";
-// import Contact from "./pages/Contact";
+import ServiceDetails from "./pages/ServiceDetails";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
 import { ContactFormProvider } from "./contexts/ContactFormContext";
 
 function App() {
@@ -17,10 +17,10 @@ function App() {
           <Navbar />
           <main className="flex-grow">
             <Routes>
-              <Route path="/EPBruh" element={<Home />} />
-              {/* <Route path="/services/:serviceId" element={<ServiceDetails />} />
+              <Route path="/" element={<Home />} />
+              <Route path="/services/:serviceId" element={<ServiceDetails />} />
               <Route path="/about" element={<About />} />
-              <Route path="/contact" element={<Contact />} /> */}
+              <Route path="/contact" element={<Contact />} />
             </Routes>
           </main>
           <Footer />
---
+++ src/pages/ServiceDetails.jsx
@@ -0,0 +1,17 @@
+// pages/Home.jsx
+import React from "react";
+import Hero from "../components/sections/Hero";
+import Services from "../components/sections/Services";
+import About from "../components/sections/About";
+import Testimonials from "../components/sections/Testimonials";
+import Contact from "../components/sections/Contact";
+
+const Home = () => {
+  return (
+    <div>
+      <Services />
+    </div>
+  );
+};
+
+export default Home;
